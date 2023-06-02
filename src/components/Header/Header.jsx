b8d4c8f42f06a2ebd2c11c87cf5405c9f1b26fa0
Fabio Lanza <fabio.lanza4@gmail.com>
2023-06-01 19:35:16 -0700

    added search

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,38 +2,82 @@ import "./Header.css";
 import { Link } from "react-router-dom";
 import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
 import { ThemeContext } from "../../Context/ThemeContext";
-import { useContext } from "react";
-import MovieLogo from '../../assets/MovieLogo.png'
+import { useContext, useEffect, useState } from "react";
+import MovieLogo from "../../assets/MovieLogo.png";
+import axios from "axios";
+import SearchResults from "../SearchResults/SearchResults";
 
-function Header() {
+function Header({ baseUrl, apiKey }) {
   const { darkMode, setDarkMode } = useContext(ThemeContext);
+  const [query, setQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
   const handleTheme = () => {
-    setDarkMode(!darkMode)
-    localStorage.setItem('darkMode', JSON.stringify(!darkMode))
-  }
+    setDarkMode(!darkMode);
+    localStorage.setItem("darkMode", JSON.stringify(!darkMode));
+  };
+
+  useEffect(() => {
+    if (query.trim().length > 0) {
+      axios
+        .get(`${baseUrl}/search/movie?api_key=${apiKey}&query=${query}`)
+        .then((res) => {
+          console.log(res.data.results)
+          setSearchResults(res.data.results);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [query]);
+
 
   return (
-    <header className={darkMode ?"header-container":"header-container header-light" }>
+    <header
+      className={
+        darkMode ? "header-container" : "header-container header-light"
+      }
+    >
       <Link to="/" className="logo">
         <img src={MovieLogo} alt="" />
       </Link>
+
       <div className="search-container">
         <input
-          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
           placeholder="Search Movies..."
           className="search-input"
         />
+
+        {query.trim() !== "" && (
+          <div className="search-results-container">
+            {searchResults.map((movie) => {
+              return (
+                <SearchResults
+                  setQuery={setQuery}
+                  key={movie.id}
+                  movie={movie}
+                />
+              );
+            })}
+          </div>
+        )} 
       </div>
+
       <div className="header-buttons-container">
         {darkMode ? (
-        <div className="theme-button-container">
-          <MdOutlineLightMode className="theme-icon " onClick={handleTheme}/>
-          <MdOutlineDarkMode className="theme-icon theme-icon-active" onClick={handleTheme} />
-        </div>
+          <div className="theme-button-container">
+            <MdOutlineLightMode className="theme-icon " onClick={handleTheme} />
+            <MdOutlineDarkMode
+              className="theme-icon theme-icon-active"
+              onClick={handleTheme}
+            />
+          </div>
         ) : (
-            <div className="theme-button-container">
-            <MdOutlineLightMode className="theme-icon theme-icon-active" onClick={handleTheme}/>
+          <div className="theme-button-container">
+            <MdOutlineLightMode
+              className="theme-icon theme-icon-active"
+              onClick={handleTheme}
+            />
             <MdOutlineDarkMode className="theme-icon " onClick={handleTheme} />
           </div>
         )}
